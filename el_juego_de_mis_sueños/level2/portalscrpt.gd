c14285966fd85928679e851f2cbd549fc471a590
Santyss3 <87153125+Santyss3@users.noreply.github.com>
2021-07-08 23:22:47 -0300

    Delete portalscrpt.gd

--- el_juego_de_mis_sueños/level2/portalscrpt.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-export(String, FILE, "*.tscn") var next_scene_path
-
-func _on_Portal_body_entered(body):
-	if body.name == "pj":
-		get_tree().change_scene("res://escenas primer mundo/+mundolevel1+.tscn")
-	pass # Reemplazar con cuerpo de función
